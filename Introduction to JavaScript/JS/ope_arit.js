037c70baaf9dfa0e21ccb169a64c47aae3b8e02a
SophrosyneEunoia <kelvinmulyawan2000@gmail.com>
2022-03-04 16:56:06 +0700

    Another Basic JavaScript

---
+++ Introduction to JavaScript/JS/ope_arit.js
@@ -0,0 +1,17 @@
+let x  = 10;
+let y = 5;
+
+console.log("Variable x :"+x);
+console.log("Variable y :"+y);
+
+let add = x + y;
+console.log("Tambah : "+add);
+
+let sub = x - y;
+console.log("Kurang : "+sub);
+
+let mul = x * y;
+console.log("Perkalian : "+mul);
+
+let div = x / y;
+console.log("Pembagian : "+div);
