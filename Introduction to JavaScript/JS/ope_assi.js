bf81434520673853375d16a8d6443a0a65017f7f
SophrosyneEunoia <kelvinmulyawan2000@gmail.com>
2022-03-04 20:43:48 +0700

    Another JavaScript Basic

---
+++ Introduction to JavaScript/JS/ope_assi.js
@@ -0,0 +1,7 @@
+let x = 10;
+let y = 5;
+
+console.log("Tambah :",x+=y)
+console.log("Kurang :",x-=y)
+console.log("Kali :",x*=y)
+console.log("Bagi :",x/=y)
---
+++ Introduction to JavaScript/JS/ope_comp.js
@@ -0,0 +1,12 @@
+let x = 10;
+let y = 12;
+let z = "10";
+
+console.log(x==y);
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log("Bonus")
+console.log(x===z);
+console.log(x==z);
